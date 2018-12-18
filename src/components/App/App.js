db189dea41cf65bb36ec6a3a6a314d0341ef5dcb
Zach Dettwiler <z.dettwiler@gmail.com>
2018-12-18 21:16:59 +0000

    basic routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react'
 import './App.css'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 
+import Home from 'components/Home/Home'
 import Exercise from 'components/Exercise/Exercise'
 import vocabDuffENTG from 'vocabDuffENTG'
 
 class App extends Component {
   render() {
     return (
-      // <BrowserRouter>
-      //   <Switch>
-      //     <Route path='/exercise' component={Exercise} />
-      //   </Switch>
-      // </BrowserRouter>
-      <div className="App">
-        <div className="Navigation">
-          <a href="">Link</a>
-        </div>
 
-        <Exercise
-          question='greek'
-          answer='english'
-          category='chapter'
-          chosenCategory={4}
-          // nbQuestions={10}
-          set={vocabDuffENTG}
-        />
+      <div className="App">
+        <BrowserRouter>
+          <div>
+
+            <div className="Navigation">
+              <Link to="/">Home</Link>
+            </div>
+
+            <Switch>
+              <Route path='/' component={Home} />
+              <Route path='/exercise' component={Exercise} />
+            </Switch>
+            
+          </div>
+        </BrowserRouter>
 
       </div>
     );
   }
 }
+
+// <Exercise
+//   question='greek'
+//   answer='english'
+//   category='chapter'
+//   chosenCategory={4}
+//   // nbQuestions={10}
+//   set={vocabDuffENTG}
+// />
 
 export default App;
