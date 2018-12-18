75807ba5bd2ff7a12d8243b68a6dd8859ea7962e
Zach Dettwiler <z.dettwiler@gmail.com>
2018-12-18 20:55:15 +0000

    installed react-router-dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import './App.css'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
 import Exercise from 'components/Exercise/Exercise'
 import vocabDuffENTG from 'vocabDuffENTG'
 
 class App extends Component {
   render() {
     return (
+      // <BrowserRouter>
+      //   <Switch>
+      //     <Route path='/exercise' component={Exercise} />
+      //   </Switch>
+      // </BrowserRouter>
       <div className="App">
         <div className="Navigation">
           <a href="">Link</a>
