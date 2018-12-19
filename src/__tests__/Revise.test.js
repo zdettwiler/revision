6b53316b26013fe3fa92c25b7b6319bc56294ae6
Zach Dettwiler <z.dettwiler@gmail.com>
2018-12-19 21:24:57 +0000

    creates array of selected chapters passed on in url

--- src/__tests__/Revise.test.js
+++ src/__tests__/Revise.test.js
@@ -6,10 +6,66 @@ import Revise from '../components/Revise/Revise'
 describe('<Revise />', () => {
   let wrapper
   beforeEach(() => {
-    wrapper = shallow(<Revise />)
+    wrapper = shallow(
+      <Revise
+        match={{ params: {} }}
+      />
+    )
   })
 
   it('renders a <div />', () => {
     expect(wrapper.find('div.Revise').length).toEqual(1)
+  })
+
+  describe('creates an exercise correctly', () => {
+
+    it('creates an array of single chapter to revise', () => {
+      let wrapper = shallow(
+        <Revise
+          match={{
+            params: { chapters: '1' }
+          }}
+        />
+      )
+
+      expect(wrapper.state('chapters')).toEqual([1])
+    })
+
+    it('creates an array of list of single chapters to revise', () => {
+      let wrapper = shallow(
+        <Revise
+          match={{
+            params: { chapters: '1,2,5,7' }
+          }}
+        />
+      )
+
+      expect(wrapper.state('chapters')).toEqual([1, 2, 5, 7])
+    })
+
+    it('creates an array of chapters to revise from range', () => {
+      let wrapper = shallow(
+        <Revise
+          match={{
+            params: { chapters: '1-4' }
+          }}
+        />
+      )
+
+      expect(wrapper.state('chapters')).toEqual([1, 2, 3, 4])
+    })
+
+    it('creates an array of chapters to revise from combination of list and range', () => {
+      let wrapper = shallow(
+        <Revise
+          match={{
+            params: { chapters: '1-3, 7, 12, 15-17' }
+          }}
+        />
+      )
+
+      expect(wrapper.state('chapters')).toEqual([1, 2, 3, 7, 12, 15, 16, 17])
+    })
+
   })
 })
--- src/components/Revise/Revise.js
+++ src/components/Revise/Revise.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react'
 
 class Revise extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      chapters: []
+    }
+
+    this.getChaptersArray = this.getChaptersArray.bind(this)
+  }
+
+  componentDidMount() {
+    if (this.props.match.params.chapters) {
+      this.getChaptersArray()
+    }
+  }
+
+  getChaptersArray() {
+    let chapters = []
+
+    // split with comma the different elements of chapter selection
+    this.props.match.params.chapters.split(',').forEach(chap => {
+      if (chap.match(/\d+-\d+/g)) { // if range
+        let boundaries = chap.match(/\d+/g).map(b => parseInt(b))
+        for (let i=boundaries[0]; i<=boundaries[1]; i++) {
+          chapters.push(i)
+        }
+      } else { // if a single chapter
+        chapters.push(parseInt(chap))
+      }
+    })
+
+    this.setState({ chapters })
+  }
+
   render() {
     return (
       <div className='Revise'>
         <h2>Revise</h2>
-        <p>{this.props.match.params.set} {this.props.match.params.chapters} {this.props.match.params.questions}</p>
-        <p>{this.props.match.params.savedEx}</p>
+        {this.props.match.params.chapters && (
+          <p>{this.state.chapters}</p>
+        )}
       </div>
     );
   }
