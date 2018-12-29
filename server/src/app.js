2d9873f60d3321cd9280699f7caf0540eca422a2
Zach Dettwiler <z.dettwiler@gmail.com>
2018-12-29 19:31:38 +0100

    more routing

--- server/src/app.js
+++ server/src/app.js
@@ -9,14 +9,14 @@ const staticFiles = express.static(path.join(__dirname, '../../client/build'))
 // pass the static files (react app) to the express app.
 app.use(staticFiles)
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.status(200).send('Hello World!')
+// })
 
 app.get('/api/revise/:set/chapters/:chapters/questions/:nbQuestions', (req, res) => {
   res.json(createExercise(req.params.set, req.params.chapters, req.params.nbQuestions))
 })
 
-app.use('/*', staticFiles)
+app.get('/*', staticFiles)
 
 export default app
