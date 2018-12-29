10c5d8f058469b72787934247b60ab2c57d1c226
Zach Dettwiler <z.dettwiler@gmail.com>
2018-12-29 19:57:10 +0100

    sendFile staticFile index.html

--- server/src/app.js
+++ server/src/app.js
@@ -4,17 +4,21 @@ import path from 'path'
 
 const app = express()
 
+console.log(path.join(__dirname, '../../client/build'))
 // get reference to the client build directory
 const staticFiles = express.static(path.join(__dirname, '../../client/build'))
 // pass the static files (react app) to the express app.
 app.use(staticFiles)
 
-app.get('/', (req, res) => {
-  res.status(200).send(staticFiles)
-})
 
+// API endpoint
 app.get('/api/revise/:set/chapters/:chapters/questions/:nbQuestions', (req, res) => {
   res.json(createExercise(req.params.set, req.params.chapters, req.params.nbQuestions))
+})
+
+// handles any other requests
+app.get('*', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../../client/build/index.html'))
 })
 
 // app.get('/*', staticFiles)
