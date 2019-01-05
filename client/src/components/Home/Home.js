29d05ad1f4493eb1a495ceed5924525fc79b323d
Zach Dettwiler <z.dettwiler@gmail.com>
2019-01-05 15:09:17 +0100

    basic form for exercise

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,6 +6,27 @@ class Home extends Component {
     return (
       <div className='Home'>
         <h1>Home</h1>
+        <select name="vocab-set">
+          <option value="greek-duff">{"Duff - Elements of New Testament Greek"}</option>
+        </select>
+        <br/>
+
+        <input type="checkbox" name="chapter" value="1" />{"1  "}
+        <input type="checkbox" name="chapter" value="2" />{"2  "}
+        <input type="checkbox" name="chapter" value="3" />{"3  "}
+        <input type="checkbox" name="chapter" value="4" />{"4  "}
+        <input type="checkbox" name="chapter" value="5" />{"5  "}
+        <input type="checkbox" name="chapter" value="6" />{"6  "}
+        <input type="checkbox" name="chapter" value="7" />{"7  "}
+        <input type="checkbox" name="chapter" value="8" />{"8  "}
+        <input type="checkbox" name="chapter" value="9" />{"9  "}
+        <input type="checkbox" name="chapter" value="10" />{"10  "}
+        <br/>
+
+        <input type="number" name="nb-questions" ></input>
+        <br/>
+
+        <button type="button">Revise</button>
       </div>
     );
   }
