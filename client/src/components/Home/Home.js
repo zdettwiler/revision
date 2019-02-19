7684cd637fced4b3f1a074260e1e06eec6d5c36c
Zach Dettwiler <z.dettwiler@gmail.com>
2019-02-19 19:54:31 +0000

    home page changes

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -9,22 +9,17 @@ class Home extends Component {
         <select name="vocab-set">
           <option value="greek-duff">{"Duff - Elements of New Testament Greek"}</option>
         </select>
-        <br/>
 
-        <input type="checkbox" name="chapter" value="1" />{"1  "}
-        <input type="checkbox" name="chapter" value="2" />{"2  "}
-        <input type="checkbox" name="chapter" value="3" />{"3  "}
-        <input type="checkbox" name="chapter" value="4" />{"4  "}
-        <input type="checkbox" name="chapter" value="5" />{"5  "}
-        <input type="checkbox" name="chapter" value="6" />{"6  "}
-        <input type="checkbox" name="chapter" value="7" />{"7  "}
-        <input type="checkbox" name="chapter" value="8" />{"8  "}
-        <input type="checkbox" name="chapter" value="9" />{"9  "}
-        <input type="checkbox" name="chapter" value="10" />{"10  "}
-        <br/>
+        Chapter
+        <select name="vocab-set">
+          <option value="1">{"1"}</option>
+        </select>
 
-        <input type="number" name="nb-questions" ></input>
-        <br/>
+
+        <select name="vocab-set">
+          <option value="5">{"5"}</option>
+        </select>
+        questions
 
         <button type="button">Revise</button>
       </div>
