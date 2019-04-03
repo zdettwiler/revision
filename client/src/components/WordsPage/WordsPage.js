38de64fde44e51186414c31cc06bac9da4bbbc14
Zach Dettwiler <z.dettwiler@gmail.com>
2019-04-03 08:34:59 +0100

    basic word stats

--- client/src/components/WordsPage/WordsPage.js
+++ client/src/components/WordsPage/WordsPage.js
@@ -11,6 +11,7 @@ class WordsPage extends Component {
       words: []
     }
     this.handleChange = this.handleChange.bind(this)
+    this.calculateStats = this.calculateStats.bind(this)
   }
 
   async componentDidMount() {
@@ -18,6 +19,23 @@ class WordsPage extends Component {
       let response = await API.findWords({ find: {} })
       this.setState({ words: response.data.words })
     } catch (err) { throw err }
+  }
+
+  calculateStats() {
+    let stats = this.state.words.reduce((stats, current) => {
+      stats[current.revisionBox] += 1
+      return stats
+
+    }, {
+      'every-day': 0,
+      'every-three-days': 0,
+      'every-week': 0,
+      'every-other-week': 0,
+      'before-test': 0
+    })
+
+    stats.total = this.state.words.length
+    return stats
   }
 
   async handleChange(e) {
@@ -34,9 +52,20 @@ class WordsPage extends Component {
   }
 
   render() {
+    let stats = this.calculateStats()
+    console.log(stats)
     return (
       <div className='WordsPage content'>
         <h1>Words</h1>
+          <ul className='stats'>
+            <li><span className={'label every-day'}>{'every-day'}</span> {stats['every-day']}</li>
+            <li><span className={'label every-three-days'}>{'every-three-days'}</span> {stats['every-three-days']}</li>
+            <li><span className={'label every-week'}>{'every-week'}</span> {stats['every-week']}</li>
+            <li><span className={'label every-other-week'}>{'every-other-week'}</span> {stats['every-other-week']}</li>
+            <li><span className={'label before-test'}>{'before-test'}</span> {stats['before-test']}</li>
+            <li><span className={'label total'}>{'total'}</span> {stats['total']}</li>
+          </ul>
+
 
 
           <table>
